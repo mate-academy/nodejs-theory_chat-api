336aa80ddc427c61932a756840dc54b455f267ff
Misha Grinko <glukym@gmail.com>
2024-07-12 18:48:14 +0300

    add long polling

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
 import express from 'express';
 import cors from 'cors';
+import EventEmitter from 'node:events';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+type Message = {
+  text: string;
+  time: Date;
+};
+
+const messages = [] as Message[];
+const messageEmitter = new EventEmitter();
+
+app.get('/messages', (req, res) => {
+  messageEmitter.once('message', () => res.send(messages));
+});
+
+app.post('/messages', (req, res) => {
+  const message = {
+    text: req.body.text,
+    time: new Date(),
+  };
+
+  messages.push(message);
+  messageEmitter.emit('message');
+  res.status(201).json(message);
 });
 
 app.listen(PORT, () => {
